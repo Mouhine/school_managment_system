21726eb5fc2d3b803bab92620bf88678788b423a
hamidovitch <hamidovitch@kali>
2024-08-10 11:06:52 -0400

    Modify the user Avatar

--- client/src/components/headnav/Avatar.tsx
+++ client/src/components/headnav/Avatar.tsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { RxAvatar } from "react-icons/rx";
-
-const Avatar = () => {
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+const UserAvatar = () => {
   return (
     <div className="flex items-center space-x-1 ">
-      <RxAvatar size={40} />
-      <div className="flex flex-col space-y-1 text-sm">
-        <span className="font-bold">Mohammed </span>
-        <span>Admin</span>
-      </div>
+      <DropdownMenu>
+            <DropdownMenuTrigger className="outline-none">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+              <DropdownMenuContent>
+              <DropdownMenuLabel>Mohamed</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem></DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     </div>
-  );
-};
+  )};
 
-export default Avatar;
+export default UserAvatar;
